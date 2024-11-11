4dcf4263e67eccc8378cebdd06b2a6d355f52152
nayem641 <nayemhossein641@gmail.com>
2024-11-11 16:30:12 +0600

    check login first

--- client/src/Pages/HomePage.jsx
+++ client/src/Pages/HomePage.jsx
@@ -6,13 +6,14 @@ import Posts from "../Components/Posts";
 import { useEffect } from "react";
 
 function HomePage() {
-  // useEffect(()=>{
-  //   const user_id=JSON.parse(localStorage.getItem("user_id"))
-  //   if(!user_id){
-  //     localStorage.removeItem("user_id")
-  //     window.location.href="/login"
-  //   }  
-  // },[])
+  const navigate=useEffect()
+  useEffect(()=>{
+    const user_id=JSON.parse(localStorage.getItem("user_id"))
+    if(!user_id){
+      localStorage.removeItem("user_id")
+      navigate("/login")
+    }  
+  },[])
   return (
     <div className="homepage">
       <Titlebar />
