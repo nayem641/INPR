8316535933cc25fbc8bb8f8ec767a947ad6503d5
nayem <nayemhossein641@gmail.com>
2024-11-16 12:55:54 +0600

    MyPost component

---
+++ client/src/Components/MyPost.jsx
@@ -0,0 +1,126 @@
+import React, { useRef, useState } from "react";
+import "./Styles/Post.css";
+import { BsThreeDots } from "react-icons/bs";
+import { BiLike } from "react-icons/bi";
+import { FaRegCommentDots } from "react-icons/fa6";
+import { PiShareFatLight } from "react-icons/pi";
+import { toast } from "react-toastify";
+
+import { useNavigate } from "react-router";
+import axios from "axios";
+
+function MyPost({ post }) {
+  const navigate = useNavigate();
+  const user_id = JSON.parse(localStorage.getItem("user_id"));
+  const [isPopupVisible, setIsPopupVisible] = useState(false);
+  const togglePopup = () => {
+    setIsPopupVisible(!isPopupVisible);
+  };
+
+  const deletePost = async (postId) => {
+    const response = await axios.delete(
+      `https://inpr.onrender.com/posts/${postId}`
+    );
+    setIsPopupVisible(false);
+    toast.success(response.data.message);
+  };
+
+  return (
+    <>
+      <div className="post-container">
+        <div className="postContainer-Uppersection">
+          <div className="post-owner">
+            <img src={post.authorPp} alt="" />
+            <div>
+              <span>
+                <a href="">{post.authorName}</a>
+              </span>
+              <p>2 days ago</p>
+            </div>
+          </div>
+
+          <div style={{ position: "relative" }}>
+            {/* Three Dots Icon */}
+            <BsThreeDots
+              onClick={togglePopup}
+              className="post-Controll"
+              style={{ cursor: "pointer" }}
+            />
+
+            {/* Popup menu */}
+            {isPopupVisible && (
+              <div className="popup-div">
+                <div className="popup-menu-div" onClick={togglePopup}>
+                  Hide Post
+                </div>
+                <div className="popup-menu-div" onClick={togglePopup}>
+                  Copy Link
+                </div>
+                <div className="popup-menu-div" onClick={togglePopup}>
+                  Follow This ID
+                </div>
+                <div className="popup-menu-div" onClick={togglePopup}>
+                  Report Post
+                </div>
+                {user_id === post.authorId && (
+                  <div
+                    className="popup-menu-div"
+                    onClick={() => {
+                      togglePopup;
+                      navigate("/updatepost", { state: post._id });
+                    }}
+                  >
+                    Edit Post
+                  </div>
+                )}
+                {user_id === post.authorId && (
+                  <div
+                    className="popup-menu-div"
+                    onClick={() => {
+                      togglePopup;
+                      deletePost(post._id);
+                    }}
+                    style={{ color: "red" }}
+                  >
+                    Delete
+                  </div>
+                )}
+              </div>
+            )}
+          </div>
+        </div>
+
+        <div className="postBody-container">
+          <div className="post-caption">{post.text && <p>{post.text}</p>}</div>
+          {post.image && (
+            <div className="post-photos">
+              {post.image && <img src={post.image} alt="" />}
+            </div>
+          )}
+          {post.video && (
+            <div style={{ padding: "5px", height: "auto" }}>
+              <video src={post.video} controls />
+            </div>
+          )}
+        </div>
+
+        <div className="post-interactions">
+          <div>
+            <BiLike className="post-interaction-icon" />
+            <span>15</span>
+          </div>
+          <div>
+            <FaRegCommentDots className="post-interaction-icon" />
+            <span>30</span>
+          </div>
+          <div>
+            <PiShareFatLight className="post-interaction-icon" />
+            <span>12k</span>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default MyPost;
--- client/src/Pages/Profile.jsx
+++ client/src/Pages/Profile.jsx
@@ -23,6 +23,7 @@ import { useNavigate } from "react-router";
 import { BiLike } from "react-icons/bi";
 import { PiShareFatLight } from "react-icons/pi";
 import { toast } from "react-toastify";
+import MyPost from "../Components/MyPost";
 
 
 //////////////////////////////////////////////
@@ -264,90 +265,91 @@ function Profile() {
             .reverse()
             .map((post) => {
               return (
-                <div className="post-container">
-                  <div className="postContainer-Uppersection">
-                    <div className="post-owner">
-                      <img src={post.authorPp} alt="" />
-                      <div>
-                        <span>
-                          <a href="">{post.authorName}</a>
-                        </span>
-                        <p>2 days ago</p>
-                      </div>
-                    </div>
+                // <div className="post-container">
+                //   <div className="postContainer-Uppersection">
+                //     <div className="post-owner">
+                //       <img src={post.authorPp} alt="" />
+                //       <div>
+                //         <span>
+                //           <a href="">{post.authorName}</a>
+                //         </span>
+                //         <p>2 days ago</p>
+                //       </div>
+                //     </div>
 
-                    <div style={{ position: "relative" }}>
-                      {/* Three Dots Icon */}
-                      <BsThreeDots
-                        onClick={togglePopup}
-                        className="post-Controll"
-                        style={{ cursor: "pointer" }}
-                      />
+                //     <div style={{ position: "relative" }}>
+                //       {/* Three Dots Icon */}
+                //       <BsThreeDots
+                //         onClick={togglePopup}
+                //         className="post-Controll"
+                //         style={{ cursor: "pointer" }}
+                //       />
 
-                      {isPopupVisible && (
-                        <div className="popup-div">
-                          <div
-                            onClick={() => {
-                              navigate("/updatepost", { state: post._id });
-                            }}
-                            className="popup-menu-div edit"
-                          >
-                            Edit
-                          </div>
-                          <div className="popup-menu-div copylink">
-                            Copy Link
-                          </div>
-                          <div
-                            className="popup-menu-div hide"
-                            onClick={() => {}}
-                          >
-                            Hide Post
-                          </div>
-                          <div
-                            className="popup-menu-div delete"
-                            onClick={() => {
-                              deletePost(post._id);
-                            }}
-                            style={{color:"red"}}
-                          >
-                            Delete
-                          </div>
-                        </div>
-                      )}
-                    </div>
-                  </div>
+                //       {isPopupVisible && (
+                //         <div className="popup-div">
+                //           <div
+                //             onClick={() => {
+                //               navigate("/updatepost", { state: post._id });
+                //             }}
+                //             className="popup-menu-div edit"
+                //           >
+                //             Edit
+                //           </div>
+                //           <div className="popup-menu-div copylink">
+                //             Copy Link
+                //           </div>
+                //           <div
+                //             className="popup-menu-div hide"
+                //             onClick={() => {}}
+                //           >
+                //             Hide Post
+                //           </div>
+                //           <div
+                //             className="popup-menu-div delete"
+                //             onClick={() => {
+                //               deletePost(post._id);
+                //             }}
+                //             style={{color:"red"}}
+                //           >
+                //             Delete
+                //           </div>
+                //         </div>
+                //       )}
+                //     </div>
+                //   </div>
 
-                  <div className="postBody-container">
-                    <div className="post-caption">
-                      {post.text && <p>{post.text}</p>}
-                    </div>
-                    {post.image && (
-                      <div className="post-photos">
-                        {post.image && <img src={post.image} alt="" />}
-                      </div>
-                    )}
-                    {post.video && (
-                      <div style={{ padding: "5px", height: "auto" }}>
-                        <video src={post.video} controls />
-                      </div>
-                    )}
-                  </div>
+                //   <div className="postBody-container">
+                //     <div className="post-caption">
+                //       {post.text && <p>{post.text}</p>}
+                //     </div>
+                //     {post.image && (
+                //       <div className="post-photos">
+                //         {post.image && <img src={post.image} alt="" />}
+                //       </div>
+                //     )}
+                //     {post.video && (
+                //       <div style={{ padding: "5px", height: "auto" }}>
+                //         <video src={post.video} controls />
+                //       </div>
+                //     )}
+                //   </div>
 
-                  <div className="post-interactions">
-                    <div>
-                      <BiLike className="post-interaction-icon" />
-                      <span>15</span>
-                    </div>
-                    <div>
-                      <FaRegCommentDots className="post-interaction-icon" />
-                      <span>30</span>
-                    </div>
-                    <div>
-                      <PiShareFatLight className="post-interaction-icon" />
-                      <span>12k</span>
-                    </div>
-                  </div>
-                </div>
+                //   <div className="post-interactions">
+                //     <div>
+                //       <BiLike className="post-interaction-icon" />
+                //       <span>15</span>
+                //     </div>
+                //     <div>
+                //       <FaRegCommentDots className="post-interaction-icon" />
+                //       <span>30</span>
+                //     </div>
+                //     <div>
+                //       <PiShareFatLight className="post-interaction-icon" />
+                //       <span>12k</span>
+                //     </div>
+                //   </div>
+                // </div>
+                <MyPost/>
               );
             })}
         {/* ----------------------------- */}
